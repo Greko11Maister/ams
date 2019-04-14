28f758ddfdab597ce0063f3052369e81524fcb10
greko11maister <greko11maister@gmail.com>
2019-04-13 23:15:05 -0500

    function  fishFood

--- src/AMS.kt
+++ src/AMS.kt
@@ -7,11 +7,26 @@ fun main(args: Array<String>) {
 
 fun feedTheFish(){
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
 
 fun randomDay() : String {
     val week = listOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]
+}
+
+fun fishFood(day : String) : String {
+
+  return  when(day) {
+        "Monday"    ->  "flakes"
+        "Tuesday"   ->  "pellets"
+        "Wednesday" ->  "redworms"
+        "Thursday"  ->  "granules"
+        "Friday"    ->  "mosquitoes"
+        "Saturday"  ->  "lettuce"
+        "Sunday"    ->  "plankton"
+        else        ->  "fasting"
+    }
+
 }
